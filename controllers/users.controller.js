2fa6523a18c0fa41c6705a1cd6ce33ad8397ae5f
anowarzz <anowarhosen444@gmail.com>
2023-03-22 12:32:14 +0600

    api created to send limited no of users based on uses query

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -16,9 +16,6 @@ module.exports.getOneRandomUser = (req, res) => {
   });
 };
 
-
-
-
 // Sending all the users when user hit the api ==> user/all
 module.exports.getAllUsers = (req, res) => {
   fs.readFile("./data/users.json", (err, data) => {
@@ -30,4 +27,31 @@ module.exports.getAllUsers = (req, res) => {
       res.status(200).send(users);
     }
   });
+};
+
+// Sending a specific number of users based on users query
+module.exports.getSpecificNumberOfUsers = (req, res) => {
+  console.log(req.query);
+
+  if (req.query.q && req.query.q > 0) {
+    const limit = req.query.q;
+
+    fs.readFile("./data/users.json", (err, data) => {
+      if (err) {
+        res.status(500).send("failed to load user");
+        console.log(err);
+      } else {
+        const users = JSON.parse(data);
+        const limitedUsers = users.slice(0, limit);
+        res.status(200).send(limitedUsers);
+      }
+    });
+  } else {
+    res.json({
+      success: false,
+      message:
+        "please enter a limit number (which is greater than 0) after the 'q'as the following example",
+      example: "/user/limit?q=5",
+    });
+  }
 };
--- routes/users.route.js
+++ routes/users.route.js
@@ -5,8 +5,8 @@ const usersController = require('../controllers/users.controller.js')
 
 
 router.get("/random", usersController.getOneRandomUser );
-
 router.get("/all", usersController.getAllUsers);
+router.get("/limit", usersController.getSpecificNumberOfUsers)
 
 module.exports = router;
 
